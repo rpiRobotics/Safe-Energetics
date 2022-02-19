cf917217f68ed2cc4a1404c74b570a9b6b56f0f5
Burak Aksoy <burakaksoy20@gmail.com>
2022-02-19 14:58:01 -0500

    debug

--- initial_setup.bash
+++ initial_setup.bash
@@ -9,9 +9,9 @@ git clone https://github.com/rpiRobotics/Safe-Energetics.git .;
 git clone -b melodic-devel https://github.com/rpiRobotics/fanuc src/testbed/fanuc
 # check build dependencies. Note: this may install additional packages,
 # depending on the software installed on the machine
-$ rosdep update
+rosdep update
 # be sure to change 'melodic' to whichever ROS release you are using
-$ rosdep install --from-paths src/ --ignore-src --rosdistro melodic
+rosdep install --from-paths src/testbed/ --ignore-src --rosdistro melodic
 
 ##############
 source /opt/ros/melodic/setup.bash;
