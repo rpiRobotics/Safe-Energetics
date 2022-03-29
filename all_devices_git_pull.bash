d0f1fbc29622816c4e4653a22fa1ff93047ca563
Burak Aksoy <burakaksoy20@gmail.com>
2022-03-29 08:26:44 -0400

    debug

--- all_devices_git_pull.bash
+++ all_devices_git_pull.bash
@@ -3,18 +3,14 @@ HOSTS=("192.168.1.100"  "192.168.1.102" )
 USERNAMES=("razer"  "oarbot_blue"  )
 PASSWORDS=("1234"  "1234")
 
-SCRIPTS=("cd ~/catkin_ws_assistive; 
+SCRIPTS=("cd ~/catkin_ws_safe_energetics; 
           git reset --hard; git pull;
-          cd ~/catkin_ws_assistive/src/kinova-ros;
-          git reset --hard; git pull;
-          cd ~/catkin_ws_assistive/src/Azure_Kinect_ROS_Driver;
+          cd ~/catkin_ws_safe_energetics/src/kinova-ros;
           git reset --hard; git pull;"
 
-          "cd ~/catkin_ws_assistive; 
+          "cd ~/catkin_ws_safe_energetics; 
           git reset --hard; git pull;
-          cd ~/catkin_ws_assistive/src/kinova-ros;
-          git reset --hard; git pull;
-          cd ~/catkin_ws_assistive/src/Azure_Kinect_ROS_Driver;
+          cd ~/catkin_ws_safe_energetics/src/kinova-ros;
           git reset --hard; git pull;")
 for i in ${!HOSTS[*]} ; do
     echo "------------"
