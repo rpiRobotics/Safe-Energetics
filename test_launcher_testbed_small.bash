526b8a7832d828031e4c8e7a5cd12f1240666b1b
Burak Aksoy <burakaksoy20@gmail.com>
2022-04-04 14:18:33 -0400

    add moveit launcher

--- test_launcher_testbed_small.bash
+++ test_launcher_testbed_small.bash
@@ -7,6 +7,9 @@ gnome-terminal --tab --title="KINOVA 1" --command "bash -c \"source ~/.bashrc; r
 
 gnome-terminal --tab --title="TFs" --command "bash -c \"source ~/.bashrc; roslaunch tf_broadcasters_testbed_small tf_broadcasters_testbed_small.launch; exec bash\"";
 
+# gnome-terminal --tab --title="KINOVA MOVEIT" --command "bash -c \"source ~/.bashrc; roslaunch j2n6s300_moveit_config j2n6s300_demo.launch; exec bash\"";
+gnome-terminal --tab --title="KINOVA MOVEIT" --command "bash -c \"source ~/.bashrc; roslaunch testbed_small j2n6s300_moveit_demo.launch; exec bash\"";
+
 sleep 3s;
 
 # gnome-terminal --tab --title="RVIZ" --command "bash -c \"source ~/.bashrc; rosrun rviz rviz -d ~/catkin_ws_safe_energetics/src/testbed_small/config/rviz/testbed_small.rviz; exec bash\"";
